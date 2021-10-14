dea2a3ef11268c20a7ee3516783b05c7c60a21ae
farouqdaif1 <daiffarouq@gmail.com>
2021-10-14 18:23:44 +0200

    Add discktop version

--- Js/menu.js
+++ Js/menu.js
@@ -1,7 +1,6 @@
-const menu =document.querySelector('.nav-b');
-const cBtn =document.querySelector('#close');
-function showMenu(){
-    document.querySelector('#close').classList.toggle('active');
-    document.querySelector('.nav-ul').classList.toggle('active');
+const menu = document.querySelector('.nav-b');
+function showMenu() {
+  document.querySelector('#close').classList.toggle('active');
+  document.querySelector('.nav-ul').classList.toggle('active');
 }
-menu.addEventListener("click",showMenu)
+menu.addEventListener('click', showMenu);
